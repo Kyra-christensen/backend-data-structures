38b48bbdbc7908b2bc603f35bf8ce6c73540d35f
Kyra Christensen <christensen.kyral@gmail.com>
2022-04-05 11:04:16 -0700

    copied the queue demo vic did

--- sandbox.js
+++ sandbox.js
@@ -36,3 +36,36 @@ console.log(stack.pop()); // 'shark'
 console.log(stack.peek()); // 'penguin'
 console.log(stack.pop()); // 'penguin'
 console.log(stack.pop()); // null
+
+
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+
+  enqueue(item) {
+    return this.queue.unshift(item);
+  }
+
+  dequeue() {
+    return this.queue.pop();
+  }
+
+  hasNext() {
+    return this.queue.length > 0;
+  }
+}
+
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue.dequeue()); // 'fox'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'goose'
+queue.enqueue('llama');
+console.log(queue.dequeue()); // 'lizard'
+console.log(queue.hasNext()); // true
+console.log(queue.dequeue()); // 'llama'
+console.log(queue.hasNext()); // false
+console.log(queue.dequeue()); // null
